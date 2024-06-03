0ceebff7c57639f8f77304cf1d06975aee86b6ee
Marcelo-Rocha-UFF <marcelo_rocha@id.uff.br>
2024-06-03 19:16:54 -0300

    Updating some comments

--- evaml-evasim-sourcecode/version2.0/evasim/json_to_evaml_conv.py
+++ evaml-evasim-sourcecode/version2.0/evasim/json_to_evaml_conv.py
@@ -1,6 +1,6 @@
 import json
 import xml.etree.ElementTree as ET
-import re # expressão regular
+import re
 from pprint import pprint
 
 script = ""
@@ -15,47 +15,47 @@ def converte(json_file_name, tkinter):
   with open(json_file_name, 'r') as openfile:
     json_object = json.load(openfile) # Is a dict.
       
-  comandos_json = json_object["data"]["node"] # Lista de nós. Cada nó (um comando) é um dict. com os seus pares chave/valor do respectivo elemento.
-  links_json = json_object["data"]["link"] # Lista de links. Cada link é um dict. com as chaves "from" e "to"
+  comandos_json = json_object["data"]["node"] # List of nodes. Each node (a command) is a dict. with their corresponding element key/value pairs.
+  links_json = json_object["data"]["link"] # List of links. Each link is a dict. with the keys "from" and "to".
 
-  # cria o elemento raiz <evaml> e seus subelementos
+  # Creates the root element <evaml> and its subelements.
   evaml_atributos = {"name":json_object["nombre"]}
   evaml = ET.Element("evaml", evaml_atributos )
   #
   settings = ET.SubElement(evaml, "settings")
-  # add os subelementos de settings com seus atributos
+  # Add the settings sub-elements with their attributes.
   for comando in comandos_json:
     voice_found = False # 
-    if comando["type"] == "voice": # busca comando voice e seus atributos
+    if comando["type"] == "voice": # Search voice command and its attributes.
       voice_atributos = {"tone":comando["voice"], "key":str(comando["key"])}
-      comandos_json.remove(comando) # exclui voice para que ele não seja processado na proxima etapa
+      comandos_json.remove(comando) # Exclude voice so that it is not processed in the next step.
       voice_found = True
       voice = ET.SubElement(settings, "voice", voice_atributos)
       break
   
-    if not voice_found: # voice precisa existir no JSON e precisa ser o primeiro elemento da VPL
-      print("Oops! The Voice element is missing...") # it's required that the voice be the first element of VPL
+    if not voice_found: # Voice must exist in JSON and must be the first element of the VPL.
+      print("Oops! The Voice element is missing...") # it's required that the voice be the first element of VPL.
       warning_message = "Sorry! I didn't find the Voice element.\n\nPlease the Voice element must be the first element of the script!\n\nThe EvaSIM will be closed!"
       tkinter.messagebox.showerror(title="Error!", message=warning_message)
       exit(1)
 
-  # este elementos ficam com os valores default pois ainda não foram implementados no robô
+  # These elements have their default values ​​as they have not yet been implemented in the robot.
   lightEffects_atributos = {"mode":"on"}
   lightEffects = ET.SubElement(settings, "lightEffects", lightEffects_atributos)
 
   audioEffects_atributos = {"mode":"on",  "vol":"100%"}
   audioEffects = ET.SubElement(settings, "audioEffects", audioEffects_atributos)
 
-  # cria as outras secões do documento EvaML
+  # Create the other sections of the EvaML document.
   script = ET.SubElement(evaml, "script")
   links = ET.SubElement(evaml, "links")
 
-  # chama as funções de processamento
-  processa_nodes(script, comandos_json, tkinter) # converte os nós json para nós XML
-  processa_links(links, links_json) # converte os links json para links XML
+  # Call processing functions
+  processa_nodes(script, comandos_json, tkinter) # Convert json nodes to XML nodes.
+  processa_links(links, links_json) # Convert json links to XML links.
 
 
-# processamentos dos comandos no arquivo json #######################################################################################
+# Processing commands in the json file #######################################################################################
 def processa_nodes(script, comandos_json, tkinter):
   for comando in comandos_json:
 
@@ -67,7 +67,7 @@ def processa_nodes(script, comandos_json, tkinter):
 
     # <motion>
     elif comando["type"] == "mov":
-      # mapeando os tipos de movimentos da cabeça
+      # Mapping the types of head movements.
       if   (comando["mov"] == "n"): motion_type = "YES"
       elif (comando["mov"] == "s"): motion_type = "NO"
       elif (comando["mov"] == "c"): motion_type = "CENTER"
@@ -94,7 +94,7 @@ def processa_nodes(script, comandos_json, tkinter):
 
     # <evaEmotion>
     elif comando["type"] == "emotion":
-      # mapeando os nomes da expressões (4 expressões)
+      # Mapping expression names (4 expressions).
       if (comando["emotion"] == "anger"): eva_emotion = "ANGRY"
       elif (comando["emotion"] == "joy"): eva_emotion = "HAPPY"
       elif (comando["emotion"] == "ini"): eva_emotion = "NEUTRAL"
@@ -105,7 +105,7 @@ def processa_nodes(script, comandos_json, tkinter):
 
     # <leds>
     elif comando["type"] == "led":
-      # mapeando os nomes da expressões (4 expressões)
+      # Mapping led animations names.
       if (comando["anim"] == "anger"): animatiom = "ANGRY"
       elif (comando["anim"] == "joy"): animatiom = "HAPPY"
       elif (comando["anim"] == "escuchaT"): animatiom = "LISTEN"
@@ -151,7 +151,7 @@ def processa_nodes(script, comandos_json, tkinter):
 
     # <counter>
     elif comando["type"] == "counter":
-      # mapping operations types
+      # Mapping operations types.
       if (comando["ops"] == "assign"): op = "="
       elif (comando["ops"] == "rest"): op = "%"
       elif (comando["ops"] == "mul"): op = "*"
@@ -165,21 +165,21 @@ def processa_nodes(script, comandos_json, tkinter):
 
     # <if>
     elif comando["type"] == "if":
-      exp_logica = comando["text"] # string com a expressao logica do condition
-      tag = "case" # temos esta variável aqui, para que, caso seja necessário, criemos o elemento default.
-      # mapping "op" types
-      if (comando["opt"]) == 4: # exact é sempre comparado com $
+      exp_logica = comando["text"] # String with the logical expression of the condition.
+      tag = "case" # we have this variable here, so that, if necessary, we create the default element.
+      # Mapping "op" types.
+      if (comando["opt"]) == 4: # Exact is always compared with $.
         var = "$"
-        op = "exact" # exact
-        value = exp_logica # neste não tem exp. logica no conteudo, só uma string
-        if (value == ""): # caso op seja exact e value "", fica definido aqui o "condicion" como <default>. Isso gera uma restrição na construção de scripts usando a VPL
+        op = "exact" #
+        value = exp_logica # There is no exp in this one. logic in the content, just a string.
+        if (value == ""): # If op is exact and value "", the "condition" is defined here as <default>. This creates a restriction on building scripts using the VPL.
           tag = "default"
-      elif (comando["opt"]) == 2: # contain com $
+      elif (comando["opt"]) == 2: # Contain with $.
         op = "contain"
         value = comando["text"]
         var = "$"
-      elif (comando["opt"]) == 5: # comparacao matemática
-        if ("==" in exp_logica): # faz o map. e retira da expressão, restando apenas os operandos separados por espaçoes vazios
+      elif (comando["opt"]) == 5: # Mathematical comparison.
+        if ("==" in exp_logica): # Make the map. and removes it from the expression, leaving only the operands separated by empty spaces.
           op = "eq"
           exp_logica = exp_logica.replace("==", "  ")
         elif (">=" in exp_logica):
@@ -198,34 +198,34 @@ def processa_nodes(script, comandos_json, tkinter):
           op = "lt"
           exp_logica = exp_logica.replace("<", "  ")
 
-        # com opt igual 5, comando["text"] tem algo desse tipo #x == 2 ou $ == 2(comparação matematica)
+        # With opt equal to 5, command["text"] has something like this #x == 2 or $ == 2 (mathematical comparison).
         if ("$" in exp_logica):
-          var = "$" # dolar é o primeiro operando.
+          var = "$" # Dollar is the first operand.
         else:
-          # a exp. logica tem um ou dois operandos do tipo #n
-          # a expressão regular retorna uma lista de #x, #y e etc
+          # The exp. logic has one or two operands of type #n.
+          # The regular expression returns a list of #x, #y, etc.
           var = (re.findall(r'\#[a-zA-Z]+[0-9]*', exp_logica)) 
         
-        if (type(var) == str): # type str indica que var é $, caso contrário var é uma lista resultante da expressao regular
-          # pegando o "value". tenta ler um numero
+        if (type(var) == str): # Type str indicates that var is $, otherwise var is a list resulting from the regular expression.
+          # Getting the "value". try to read a number.
           value = (re.findall(r'[0-9]+', exp_logica))
-          if (len(value) == 0): # não encontrou um numero na expressao
-            # se não é um captura a var #n no oprando da direita
-            value = re.findall(r'\#[a-zA-Z]+[0-9]*', exp_logica)[0] # captura a variavel do tipo #n e coloca em value. neste caso precisa ir com #
-          else: # encontrou o numero. lembrando que a exp. regular retorna uma lista
-            value = value[0] # entao value[0] é a string retornada pela exp. regular
-        else: # neste caso $ não está na exp. e var é um lista. precisamos verificar se tem um ou dois elementos
+          if (len(value) == 0): # Did not find a number in the expression.
+            # If it is not a capture the var #n on the right.
+            value = re.findall(r'\#[a-zA-Z]+[0-9]*', exp_logica)[0] # Captures the variable of type #n and puts it in value. in this case you need to go with #.
+          else: # Found the number. Remembering that exp. regular returns a list.
+            value = value[0] # So value[0] is the string returned by exp. regular.
+        else: # In this case $ is not in exp. and var is a list. we need to check if it has one or two elements.
           print(len(var))
-          if (len(var) == 1): # o operando da esquerda é uma var #n
-            var = var[0][1:] # pegamos a variavle sem o #
-            value = (re.findall(r'[0-9]+', exp_logica))[0] # neste caso value só pode ser um número
-          else: # var tem dois elementos, ou seja, os dois operandos sao do tipo #n #m
-            var = var[0][1:] # pegamos a variable sem o #
-            value = var[1] # uma var em value precisa ir com o caracter #
+          if (len(var) == 1): # The left operand is a var #n.
+            var = var[0][1:] # We get the variable without the #.
+            value = (re.findall(r'[0-9]+', exp_logica))[0] # In this case value can only be a number.
+          else: # Var has two elements, that is, the two operands are of type #n #m.
+            var = var[0][1:] # We get the variable without the #.
+            value = var[1] # A var in value must go with the character #.
       if_atributos = {"key" : str(comando["key"]), "op" : op, "value" : value, "var" : var}
       ET.SubElement(script, tag , if_atributos)
 
-    # Todos os comandos suportados foram testados
+    # All supported commands have been tested.
     else:
       warning_message = """Sorry, an unsupported VPL element was found. Please, check your JSON script!
 
@@ -253,16 +253,16 @@ The EvaSIM will be closed..."""
       exit(1)
 
 
-# processamentos dos links no arquivo json #######################################################################################################
+# Processing links in the json file #######################################################################################################
 def processa_links(links, links_json):
   for link in links_json:
     link_atributos = {"from" : str(link["from"]), "to" : str(link["to"])}
     ET.SubElement(links, "link", link_atributos)
 
-  # gera o arquivo XML no disco
-  xml_processed = ET.tostring(evaml, encoding="unicode")#.decode('utf8')
+  # Generate the XML file on disk.
+  xml_processed = ET.tostring(evaml, encoding="unicode")
   print("Processando XML..............")
-  with open("_json_to_evaml_converted.xml", "w") as text_file: # grava o xml processado (temporario) em um arquivo para ser importado pelo parser
+  with open("_json_to_evaml_converted.xml", "w") as text_file: # Writes the processed xml (temporary) to a file to be imported by the parser.
       text_file.write(xml_processed)
 
 
